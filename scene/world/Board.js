cff25c9812e5bedad85f434cc323f6aaa58dc21a
the-other-clara <clara.lautre@edu.gobelins.fr>
2022-10-19 15:10:05 +0200

    DRAFT: Badge shader

--- scene/world/Board.js
+++ scene/world/Board.js
@@ -1,6 +1,8 @@
-import { Mesh, MeshBasicMaterial, Object3D, BoxGeometry, PlaneGeometry, DoubleSide } from 'three'
+import { Mesh, MeshBasicMaterial, Object3D, BoxGeometry, PlaneGeometry, ShaderMaterial, TextureLoader, Vector2, Vector3 } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 let boardURL = require('../../assets/models/board.glb')
+let stickers = require('../../assets/img/stickers.png')
+let stickersMask = require('../../assets/img/stickers_mask.png')
 import gsap from 'gsap'
 
 
@@ -33,14 +35,57 @@ export class Board {
   }
   setBoardOverlay() {
     this.overlayGeometry = new PlaneGeometry( 2, 5 )
-    this.overlayMaterial = new MeshBasicMaterial( {color: 0xffffff, side: DoubleSide, transparent: true, opacity:0.6} )
-    this.overlay = new Mesh( this.overlayGeometry, this.overlayMaterial )
+    this.maskMaterial = new ShaderMaterial({
+      transparent: true,
+      uniforms: {
+         activated: {type : "v2v", value : new Vector3(0.2, 0.2, 0.2)},
+         stickerT : {value : this.stickersText},
+         stickerM : {value : this.stickersMask},
+      },
+      vertexShader: `
+      varying vec2 vUv;
+      void main() {
+          vUv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+  `,
+      fragmentShader: `
+      varying vec2 vUv;
+
+      uniform vec3 activated;
+      uniform sampler2D stickerT;
+      uniform sampler2D stickerM;
+
+      void main() {
+          vec2 uv = vUv;
+          vec4 sticker = texture2D(stickerT, uv).rgba;
+          vec3 mask = texture2D(stickerM, uv).rgb;
+
+          float sticker1 = 1. - step(0.02, distance(mask.r, 1.));
+          sticker1 *= activated.r;
+          float sticker2 = 1. - step(0.02, distance(mask.r, 0.66));
+          sticker2 *= activated.g;
+          float sticker3 = 1. - step(0.02, distance(mask.r, 0.33));
+          sticker3 *= activated.b;
+          
+          vec3 color = vec3(0.);
+          color = (sticker1 + sticker2 + sticker3) * sticker.rgb;
+          gl_FragColor = vec4(color, sticker.a);
+      }
+    `,
+    })
+    
+    new TextureLoader().load(stickers, (tex) => {
+      this.maskMaterial.uniforms.stickerT.value = tex
+    })
+    new TextureLoader().load(stickersMask, (tex) => {
+      this.maskMaterial.uniforms.stickerM.value = tex
+    })
+    this.overlay = new Mesh( this.overlayGeometry, this.maskMaterial )
     this.overlay.name = "Overlay"
     this.overlay.rotation.x = Math.PI / 2
     this.overlay.position.y = 0.68
     this.container.add(this.overlay)
-
-    
   }
   easeInBounce(x) {
     return 1 - this.easeOutBounce(1 - x);
